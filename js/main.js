59581d8dede02a21246a133be3cb8272f79a28cc
Vinicius Valadares Chinelatto <103156091+Vaaladares@users.noreply.github.com>
2024-04-22 22:48:37 -0300

    atualizando o header mobile

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.gallery-container').slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 2,
         dots: true,
         autoplay: true,
-        autoSpeed: 2000, 
+        autoSpeed: 2000,
         speed: 900
     });
-  });
+});
 
-  let button = document.querySelector(".fa-bars")
-  let menuMobile = document.querySelector(".navbar-itens-mobile")
+let button = document.querySelector(".fa-bars")
+let menuMobile = document.querySelector(".navbar-itens-mobile")
 
-  button.addEventListener('click', function() {
-    if(menuMobile.classList.contains('open-mobile')) {
+button.addEventListener('click', function () {
+    if (menuMobile.classList.contains('open-mobile')) {
         menuMobile.classList.remove('open-mobile')
     } else {
         menuMobile.classList.add('open-mobile')
     }
-  })
+})
+
+document.addEventListener('click', function(e) {
+    let el = e.target;
+    
+    if(el.classList.contains('botao-li')) {
+        
+        if (menuMobile.classList.contains('open-mobile')) {
+            menuMobile.classList.remove('open-mobile')
+        } else {
+            menuMobile.classList.add('open-mobile')
+        };
+    };    
+});
+
+
